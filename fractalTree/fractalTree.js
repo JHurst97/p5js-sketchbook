7f61400d8cfb42fe62aa5f4d7f8b9481441c79ac
VenomSwitch <37871254+VenomSwitch@users.noreply.github.com>
2020-01-27 16:47:00 +0000

    Update fractalTree.js

--- fractalTree/fractalTree.js
+++ fractalTree/fractalTree.js
@@ -1,34 +1,35 @@
 function setup() {
   createCanvas(800, 650);
+  // create sliders
+  aSlider = createSlider(-5, 5, 4.5,0.01); //angle
+  aSlider.position(20, 20);
+  dSlider = createSlider(0, 255, 50); //depth/length
+  dSlider.position(20, 50);
+  bSlider = createSlider(0, 5, 2); //branch count
+  bSlider.position(20, 80);
 }
+
+let aSlider, dSlider, bSlider;
 
 function draw() {
   background(234);
+  text('angle', aSlider.x * 2 + aSlider.width, 35);
+  text('depth/length', dSlider.x * 2 + dSlider.width, 65);
+  text('branch density', bSlider.x * 2 + bSlider.width, 95);
   translate(width/2, height);
-
-  branch(100);
+  branch(dSlider.value());
 }
 
 function branch(len) {
   line(0, 0, 0, -len);
-  
-    translate(0,-len);
+  translate(0, -len);
   if (len >15) {
-    push();
-    rotate(0.5);
-    branch(len * 0.75);
-    pop();
-    push();
-    rotate(-0.5);
-    branch(len * 0.75);
-    pop();
-    push();
-    rotate(0.1);
-    branch(len * 0.75);
-    pop();
-    push();
-    rotate(-0.2);
-    branch(len * 0.75);
-    pop();
+    for (let i = 0; i < bSlider.value(); i++) {
+      let angle = map(i, 0, 5, -0.5, aSlider.value());
+      push();
+      rotate(angle);
+      branch(len * 0.75);
+      pop();
+    }
   }
 }
